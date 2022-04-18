844deea6be28c448f214442d329aa8c5100f1c82
Raul Bautista <rbmolina27@gmail.com>
2022-04-18 19:50:44 +0200

    no message

---
+++ MoviesApp/MoviesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviesApp
+//
+//  Created by Raul Bautista on 18/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
