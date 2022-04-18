896d4e24b2cd7383ac758342a1ecdf69fcac422c
Raul Bautista <rbmolina27@gmail.com>
2022-04-18 20:08:54 +0200

    objcts storyboard

--- MoviesApp/MoviesApp/View/ViewController.swift
+++ MoviesApp/MoviesApp/View/ViewController.swift
@@ -7,7 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    
+    
+    @IBOutlet weak var movieList: UITableView!
+    @IBOutlet weak var favList: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,5 +20,17 @@ class ViewController: UIViewController {
     }
 
 
+}
+// MARK: - Table Controller
+extension ViewController {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
 }
 
